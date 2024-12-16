1ddf4634564a7d6db2557e30b99e4915acaf6a2d
Raul Marco Cano <rawraul@outlook.com>
2024-12-16 09:51:08 +0000

    Auth

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, MaxLength, MinLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  //   message: 'Password too weak',
+  // })
+  password: string;
+}
---
+++ src/auth/users.repository.ts
@@ -0,0 +1,20 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.userRepository.create({ username, password });
+    await this.userRepository.save(user);
+  }
+}
