125d7be7b95ae3fe49074e1c16f076333c16207e
Raul Marco Cano <rawraul@outlook.com>
2024-12-24 13:14:30 +0100

    bcrypt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,18 +3,27 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersRepository {
+export class UsersRepository extends Repository<User> {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
-  ) {}
+  ) {
+    super(User, userRepository.manager);
+  }
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = this.userRepository.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
     await this.userRepository.save(user);
   }
 }
